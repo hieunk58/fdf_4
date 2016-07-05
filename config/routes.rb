afa39127b9a94dbaf1c4ff14a0ee8e427c161fb5
hieunk58 <khachieunk@gmail.com>
2016-07-05 10:59:56 +0700

    user filter food and drink

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
   resources :products, only: [ :index, :show]
-
   get "users/new"
   resources :users do
     resources :orders, only: [:index]
   end
 
-  root "static_pages#home"
+  root "products#index"
   get "home" =>"static_pages#home"
   get "signup" => "users#new"
   get "signin"  => "sessions#new"
   post "signin" => "sessions#create"
   delete "signout" => "sessions#destroy"
+
+  resources :users
   resources :comments, only: [:create, :new, :show]
   resources :suggests, only: [:index, :create]
   resource :order, only: [:show, :update, :index]
   resources :order_details, only: [:show, :create, :update, :destroy]
+  resources :products, only: [ :index, :show]
 
   namespace :admin do
     root "products#index"
